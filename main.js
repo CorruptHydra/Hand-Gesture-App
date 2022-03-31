2da02c312b59cee8bc5a46f1d1f49a203f520f76
CorruptHydra <88399305+CorruptHydra@users.noreply.github.com>
2022-03-30 20:41:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+prediction_1=""
+predection_2=""
+
+
+Webcam.set({
+width:350,
+height:300,
+image_format: 'png',
+png_quality:90
+});
+
+ camera=document.getElementById("camera");
+
+ Webcam.attach('#camera')
+
+ function take_snapshot(){
+     Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src= "'+data_uri+'"/>';
+     });
+ }
+
+    console.log('ml5 version:', ml5.version);
+
+ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/4HLXx_vEH/model.json', modelLoaded);
+
+    function modelLoaded() {
+        console.log('Model Loaded!');
+    }
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + predection_1;
+    speak_data_2 = "The second prediction is " + predection_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+  img = document.getElementById('captured_image');
+  classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_gesture_name").innerHTML = results[0].label;
+      document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+      prediction_1 = results[0].label;
+      prediction_2 = results[1].label;
+      speak();
+      if(results[0].label == "nice")
+      {
+          document.getElementById("update_gesture").innerHTML = "&#128076;";
+      }
+      if(results[0].label == "high-five")
+      {
+          document.getElementById("update_gesture").innerHTML = "&#128075;";
+      }
+      if(results[0].label == "point")
+      {
+          document.getElementById("update_gesture").innerHTML = "&#9757;";
+      }
+  
+      if(results[1].label == "nice")
+      {
+          document.getElementById("update_gesture2").innerHTML = "&#128076;";
+      }
+      if(results[1].label == "high-five")
+      {
+          document.getElementById("update_gesture2").innerHTML = "&#128075;";
+      }
+      if(results[1].label == "point")
+      {
+          document.getElementById("update_gesture2").innerHTML = "&#9757;";
+      }
+    }
+  }
